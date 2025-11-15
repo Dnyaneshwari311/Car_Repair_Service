4b4f5e7646b43936a1abe35507bf89ccc74bca0a
Abhishek Wadhai <abhishek.wadhai@excellminds.com>
2025-11-15 14:53:42 +0530

    update login status code

--- car_repair_service/api/login.py
+++ car_repair_service/api/login.py
@@ -211,7 +211,7 @@ def login_and_get_token():
 
     except frappe.AuthenticationError:
         print("i ex....")
-        frappe.local.response["http_status_code"] = 403
+        frappe.local.response["http_status_code"] = 401
         frappe.local.response["message"] = "Invalid login credentials"
         return
         # return {"status": "error", "message": _("Invalid login credentials")}
