91707e33d1c4510c7b4f3684392ec37192c00c00
Abhishek Wadhai <abhishek.wadhai@excellminds.com>
2026-01-12 10:15:03 +0530

    update assigend to according

--- car_repair_service/api/quotation_a_r.py
+++ car_repair_service/api/quotation_a_r.py
@@ -589,7 +589,8 @@ def create_or_update_car_repair(q):
 
         child_rows.append({
             "damage_description": desc,
-            "assigned_to": row.get("assigned_to") or "",
+            # "assigned_to": row.get("assigned_to") or "",
+             "assigned_to": None,
             "part_required": row.get("part_required") or "",
             "quantity": qty,
             "estimated_cost": rate,
@@ -715,9 +716,8 @@ def create_or_update_car_repair(q):
         "list_of_damage": [],
         "vehicle_concern": []
     })
-
-    repair.insert(ignore_permissions=True)
-
+    
+    
     for row in child_rows:
         repair.append("list_of_damage", row)
 
@@ -727,6 +727,19 @@ def create_or_update_car_repair(q):
     repair.save(ignore_permissions=True)
     for d in repair.list_of_damage:
       d.assigned_to = None
+
+
+    repair.insert(ignore_permissions=True)
+
+    # for row in child_rows:
+    #     repair.append("list_of_damage", row)
+
+    # for row in concern_rows:
+    #     repair.append("vehicle_concern", row)
+
+    # repair.save(ignore_permissions=True)
+    # for d in repair.list_of_damage:
+    #   d.assigned_to = None
 
     repair.db_update()
     frappe.db.commit()
